bb12857a12f3dbf64d10cb6a7c8e1a62bc60eee6
Grigory Syrkin <gregorysyrkin@icloud.com>
2016-12-16 08:16:49 +0700

    OK

--- Project2.js
+++ Project2.js
@@ -20,22 +20,30 @@ IPs = IPs.filter (function (value, index, array) {
 
 var regExpMask = /[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\./g
 
-console.log(IPs);
+// console.log(IPs);
 var ifNeedsPrinting = true;
 for (var i = 0; i < IPs.length; i++) {
 	var maskCurr = IPs[i].match(regExpMask);
+	var maskNext = IPs[i + 1].match(regExpMask);
+	
+	maskCurr = String(maskCurr);
+	maskNext = String(maskNext);
+	
+	// console.log(typeof (maskCurr));
+	// console.log(typeof (maskNext));
+	
+	// console.log(maskCurr == maskNext);
+	// console.log(maskCurr, maskNext);
+	if (ifNeedsPrinting == true) {console.log(IPs[i])}
 
-		var maskNext = IPs[i + 1].match(regExpMask);
-		// console.log(maskCurr, maskNext);
-		if (ifNeedsPrinting == true) {console.log(IPs[i])}
-		if (maskCurr == maskNext) {
-			console.log('FLAG');
-			console.log(IPs[i + 1])
-			ifNeedsPrinting = false;
-		}
-		else {
-			console.log(i);
-			// console.log('');
-			ifNeedsPrinting = true;
-		} 
+	if (maskCurr == maskNext) {
+		// console.log('FLAG');
+		console.log(IPs[i + 1])
+		ifNeedsPrinting = false;
+	}
+	else {
+		console.log(i);
+		// console.log('');
+		ifNeedsPrinting = true;
+	} 
 }
